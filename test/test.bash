040211317841e64e269541059728bff938ebd9c2
Orbital-G <agnica0404@gmail.com>
2024-01-18 18:14:58 +0900

    add sleep command and indention option, change return result

--- test/test.bash
+++ test/test.bash
@@ -2,6 +2,7 @@
 # SPDX-FileCopyrightText: 2024 Kadono Kodai
 # SPDX-License-Identefier: BSD-3-Clause
 
+
 dir=~
 [ "$1" != "" ] && dir="$1"
 
@@ -9,7 +10,13 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 
-(echo "neko\n" | ros2 run ReverseString enter_string )
-timeout 10 ros2 run ReverseString reverse_string > /tmp/ReverseString.log
+(echo -e "neko\n" | ros2 run ReverseString enter_string > /tmp/original.log 2>&1) &
+sleep 10
 
-(cat /tmp/ReverseString.log | grep 'Reversed: oken')
+(ros2 run ReverseString reverse_string > /tmp/reversed.log 2>&1) &
+sleep 10
+
+(res=0; cat /tmp/reversed.log | grep -a 'neko' || { echo "NG: neko not found"; res=1; })
+
+[ "$res" = 0 ] && echo "OK" || echo "Test Failed"
+exit $res
