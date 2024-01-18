b386931addb4b69e4837900835bfea790afc2ba2
Orbital-G <agnica0404@gmail.com>
2024-01-18 19:36:36 +0900

    fix wrong grep option and correct command

--- test/test.bash
+++ test/test.bash
@@ -2,7 +2,6 @@
 # SPDX-FileCopyrightText: 2024 Kadono Kodai
 # SPDX-License-Identefier: BSD-3-Clause
 
-
 dir=~
 [ "$1" != "" ] && dir="$1"
 
@@ -10,13 +9,12 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 
+(ros2 run ReverseString reverse_string > /tmp/reversed.log 2>&1) &
+
 (echo -e "oken\n" | ros2 run ReverseString enter_string > /tmp/original.log 2>&1) &
 sleep 10
 
-(ros2 run ReverseString reverse_string > /tmp/reversed.log 2>&1) &
-sleep 10
-
-(res=0; cat /tmp/reversed.log | grep -a 'neko' || { echo "NG: neko not found"; res=1; })
+(res=0; cat /tmp/reversed.log | grep -q 'Reversed String: neko' || { echo "NG: neko not found"; res=1; })
 
 [ "$res" = 0 ] && echo "OK" || echo "Test Failed"
 exit $res
