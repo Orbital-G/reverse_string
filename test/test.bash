3518e12f9797a30d1f8213812ef915599e2a9b0a
Orbital-G <agnica0404@gmail.com>
2024-01-20 12:18:34 +0900

    do long sleep time

--- test/test.bash
+++ test/test.bash
@@ -10,10 +10,10 @@ colcon build
 source $dir/.bashrc
 
 (ros2 run reverse_string reverse_string > /tmp/reversed.log 2>&1)&
-sleep 5
+sleep 10
 
-(echo -e "oken\n" | ros2 run reverse_string enter_string)&
-sleep 5
+(echo -e "oken\n" | ros2 run reverse_string enter_string > /tmp/original.log 2>&1)&
+sleep 10
 
 cat /tmp/reversed.log
 # | grep -a 'Reversed String: neko' 
