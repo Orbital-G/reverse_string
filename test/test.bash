756414ff35844418359a21ddaea6e8c0566345ad
Orbital-G <agnica0404@gmail.com>
2024-01-20 13:58:42 +0900

    revert moving check topic and add redirect log

--- test/test.bash
+++ test/test.bash
@@ -9,12 +9,13 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 
-(ros2 run reverse_string reverse_string )&
-sleep 5
+(ros2 run reverse_string reverse_string > /tmp/reversed.log 2>&1)&
+sleep 10
 
 jobs
 
-(echo -e "oken\n" | ros2 run reverse_string enter_string )&
+ros2 topic list | grep original_string
+(echo -e "oken\n" | ros2 run reverse_string enter_string > /tmp/original.log 2>&1)&
 sleep 10
 
 jobs
