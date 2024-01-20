58088cdeb8b3a9f137e37c150a37df964cb73077
Orbital-G <agnica0404@gmail.com>
2024-01-20 13:26:43 +0900

    check jobs

--- test/test.bash
+++ test/test.bash
@@ -9,11 +9,15 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 
-(ros2 run reverse_string reverse_string)&
-sleep 20
+(ros2 run reverse_string reverse_string )&
+sleep 10
 
-(echo -e "oken\n" | ros2 run reverse_string enter_string)&
-sleep 20
+(echo -e "oken\n" | ros2 run reverse_string enter_string )&
+sleep 10
+
+jobs
+
+fg %1
 
 cat /tmp/reversed.log
 # | grep -a 'Reversed String: neko' 
