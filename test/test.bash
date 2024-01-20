1bdfed2cca89cd75246f88560bf0bab71e88e8f9
Orbital-G <agnica0404@gmail.com>
2024-01-20 14:23:23 +0900

    add result comments and fix spell miss

--- test/test.bash
+++ test/test.bash
@@ -12,11 +12,17 @@ source $dir/.bashrc
 (ros2 run reverse_string reverse_string > /tmp/reversed.log 2>&1)&
 sleep 10
 
-ros2 topic list | grep origi
+ros2 topic list | grep original_string
 (echo -e "oken\n" | ros2 run reverse_string enter_string > /tmp/original.log 2>&1)&
 sleep 10
 
 sleep 5
 jobs
 
-cat /tmp/reversed.log | grep -a 'Reversed String: neko' 
+if ! cat /tmp/reversed.log | grep -q 'Reversed String: neko'; then
+  echo "NG: neko not found"
+  exit 1
+else
+  echo "OK"
+  exit 0
+fi
